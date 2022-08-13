ff001524145ea5a06f705f016dcf439d28fe2e2c
Yogesh <virus2.0.exe@gmail.com>
2022-08-13 22:22:54 +0530

    learned about String Concatenation in JAVASCRIPT

---
+++ JAVASCRIPT/file9.js
@@ -0,0 +1,12 @@
+// String Concatenation
+
+let string1="yogesh";
+let string2="kaushik";
+
+let fullName=string1+" "+string2;
+console.log(fullName);
+
+let string3="10";
+let string4="17";
+let newstring= +string3 + +string4
+console.log(newstring);
