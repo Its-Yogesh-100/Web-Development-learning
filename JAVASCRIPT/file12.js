f3de85ce05ddba5634c18d48aadf4f9714f19f29
Yogesh <virus2.0.exe@gmail.com>
2022-08-14 00:57:40 +0530

    learn about boolean, equal ,not equal operator

---
+++ JAVASCRIPT/file12.js
@@ -0,0 +1,33 @@
+// Booleans  and Comparision Operator
+
+// booleans
+// true , false
+
+let n1=40;
+let n2=50;
+
+console.log(n1>n2);
+
+// == only check value not data type
+//  for == "1" and 1 are same it will return 
+// true value
+
+let n11="1"
+let n22=1
+console.log(n11==n22);
+
+// for comparisioin of data type and value
+// so we use ===
+
+console.log(n11===n2);
+
+
+// not equal to
+
+
+let kk='7';
+let kkk=7;
+console.log(kk!=kkk);
+
+// for not equal we use ! = =
+console.log(kk!==kkk);
