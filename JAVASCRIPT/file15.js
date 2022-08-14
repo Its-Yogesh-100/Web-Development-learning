25dd151d60cd0e1c1402824181788821a166ab55
Yogesh <virus2.0.exe@gmail.com>
2022-08-14 15:33:34 +0530

    Learn about Ternary Operator

---
+++ JAVASCRIPT/file15.js
@@ -0,0 +1,22 @@
+// ternary operator in javascript
+
+
+let age=20;
+let drink;
+
+if(age<=5)
+{
+    drink="milk";
+    console.log(drink);
+}
+else
+{
+    drink="coffee";
+    console.log(drink);
+}
+
+// insist of this we can use ternary operator
+
+let a=0;
+let d=a>=20? "coffee":"milk";
+console.log(d);
