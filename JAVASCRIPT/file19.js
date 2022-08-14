7728a6a8b83741f2dcda75fc666306844b9f66c5
Yogesh <virus2.0.exe@gmail.com>
2022-08-14 16:27:21 +0530

    Learned About Switch Cases

---
+++ JAVASCRIPT/file19.js
@@ -0,0 +1,36 @@
+// SWITCH STATEMENT
+
+let day= +prompt("Enter the number between 0 and 6 both included ");
+
+switch (day) {
+  case 0:
+    console.log(" SUNDAY ");
+    break;
+
+  case 1:
+    console.log("MONDAY");
+    break;
+
+  case 2:
+    console.log(" TUESDAY ");
+    break;
+
+  case 3:
+    console.log("WEDNESDAY");
+    break;
+
+  case 4:
+    console.log(" THRUSDAY ");
+    break;
+
+  case 5:
+    console.log(" FRIDAY ");
+    break;
+
+  case 6:
+    console.log(" Saturday ");
+    break;
+
+    default:
+        console.log("LOSER");
+}
