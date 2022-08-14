537b8e7e5e3bebf0749f5c88b598901c50d4d833
Yogesh <virus2.0.exe@gmail.com>
2022-08-14 16:19:04 +0530

    Learned about IF , ELSEIF,ELSE

---
+++ JAVASCRIPT/file18.js
@@ -0,0 +1,14 @@
+// if
+// else if
+//else
+
+let temp=45;
+
+if(temp<0)
+ console.log("!!! EXTREMLY COLD 🥶🥶🥶");
+else if(temp<16)
+ console.log("!! Too cold ☃️☃️☃️☃️☃️☃️☃️");
+else if(temp>16 && temp<25)
+  console.log("!!! Good Temperatur 🌨️🌨️🌨️🌨️🌨️🌨️🌨️");
+else
+ console.log("!!!! SUMMER !!! 🤣🤣");
