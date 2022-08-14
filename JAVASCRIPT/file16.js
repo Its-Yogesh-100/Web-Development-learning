c502668c98ceefa8fd81a9e0c36902bd0fde2e5f
Yogesh <virus2.0.exe@gmail.com>
2022-08-14 15:38:24 +0530

    Learned about and and or

---
+++ JAVASCRIPT/file16.js
@@ -0,0 +1,13 @@
+// and and or operator
+
+let age=250;
+let name="yogesh";
+
+if(name[0]=='y' && age>20)
+{
+    console.log("YOu are adult your age is ",age);
+}
+else
+{
+    console.log("You are not adult ");
+}
