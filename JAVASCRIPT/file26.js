d097a57fcad9398f5824424428c81d164a42e29c
Its-Yogesh-100 <virus2.0.exe@gmail.com>
2022-08-18 16:49:52 +0530

    ARRAY IN JAVAS

---
+++ JAVASCRIPT/file26.js
@@ -0,0 +1,32 @@
+// INTRO TO ARRAYS
+
+// reference type
+// arrays are mutable
+// means that you can change the value  of array
+
+
+// array is object in java script
+// HOW TO CREATE ARRAYS
+
+
+// collectiohn of similar data type
+
+let alpha=["10","hello","world"];
+
+let number=[1,2,3,4];
+
+let fixed=[1,2,3,4 ,null,"string"]
+
+console.log(fixed);
+console.log(alpha);
+console.log(number);
+
+alpha[0]=100;
+console.log(alpha);
+
+console.log(typeof alpha);
+
+// below function will tell you wheter the given data type is array or not
+
+console.log(Array.isArray(alpha)); 
+
