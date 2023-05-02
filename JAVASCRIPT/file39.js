bb1b6b499c64bb458de3d8b8557080c7c4c61ad6
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-05-02 20:21:03 +0530

    some learning

---
+++ JAVASCRIPT/file39.js
@@ -0,0 +1,25 @@
+// Computed Properties
+
+const key1="Object11";
+const key2="object12";
+
+const value1="val1";
+const value2="val2";
+
+
+// we can use [ ] for inputing the value of key1 and key2
+
+const obj={
+
+    [key1]:value1,
+    [key2]:value2
+}
+// another way of createing this
+
+const obb={};
+obb[key1]=value1;
+obb[key2]=value2;
+console.log(obj);
+
+
+
